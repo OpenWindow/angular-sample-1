1700bc32452611379a5e42ffb2f547df7afe8d7c
Neel Mallepally <neel.mallepally@aimia.com>
2021-02-03 18:35:10 -0500

    added tailwind and sample code

--- sample1/src/app/app.component.ts
+++ sample1/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sample1';
+export class AppComponent implements OnInit {
+  
+  title = 'Sample';
+  categories =[];
+  categoryItems = [];
+
+  ngOnInit(): void {
+    this.categories = [
+      {
+      name : "category 1",
+      items: [ { label: "cat1-item1", description: "blah blah"}, { label: "cat1-item2", description: "blah blah"}, { label: "cat1-item3", description: "blah blah"}]
+    },
+    {
+      name : "category 2",
+      items: [ { label: "cat2-item1", description: "blah blah"}, { label: "cat2-item2", description: "blah blah"}, { label: "cat2-item3", description: "blah blah"}]
+    },
+    {
+      name : "category 3",
+      items: [ { label: "cat3-item1", description: "blah blah"}, { label: "cat2-item2", description: "blah blah"}, { label: "cat3-item3", description: "blah blah"}]
+    },
+    {
+      name : "category 4",
+      items: [ { label: "cat4-item1", description: "blah blah"}, { label: "cat3-item2", description: "blah blah"}]
+    },
+    {
+      name : "category 5",
+      items: [ { label: "cat5-item1", description: "blah blah"}, ]
+    }];
+
+    this.categoryItems = this.categories[0].items;
+  }
+
+  onCategorySelect(itemIndex) {
+    this.categoryItems = this.categories[itemIndex].items;
+  }
+
 }
